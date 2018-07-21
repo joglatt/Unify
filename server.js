208a73a477c7500e250eee2835a90f4f9a17bbe4
joglatt <34425753+joglatt@users.noreply.github.com>
2018-07-21 14:19:06 -0400

    all new stuff

--- server.js
+++ server.js
@@ -1,30 +1,44 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const session = require('express-session')
+const dbConnection = require('./config/database') 
+const MongoStore = require('connect-mongo')(session)
+const passport = require('./config/passport');
+const app = express()
 const PORT = process.env.PORT || 3001;
 
-// Configure body parser for AJAX requests
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-// Serve up static assets
-app.use(express.static("client/build"));
+// Route requires
+const user = require('./config/routes/user')
 
-// Add routes, both API and view
-require('./routes')(app);
+// MIDDLEWARE
+app.use(morgan('dev'))
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+)
+app.use(bodyParser.json())
 
-// Set up promises with mongoose
-mongoose.Promise = global.Promise;
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/login_demo",
-  {
-    useMongoClient: true
-  }
-);
+// Sessions
+app.use(
+	session({
+		secret: 'kevin_ferguson', //pick a random string to make the hash that is generated secure
+		store: new MongoStore({ mongooseConnection: dbConnection }),
+		resave: false, //required
+		saveUninitialized: false //required
+	})
+)
 
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+// Passport
+app.use(passport.initialize())
+app.use(passport.session()) // calls the deserializeUser
+
+
+// Routes
+app.use('/user', user)
+
+// Starting Server 
+app.listen(PORT, () => {
+	console.log(`App listening on PORT: ${PORT}`)
+})
