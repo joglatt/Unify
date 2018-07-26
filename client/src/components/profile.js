76d6858f75f0aff5c02dc5242bcb31f803d581e7
Michael Myers <myersmyer@gmail.com>
2018-07-25 20:03:59 -0400

    logging user

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -1,24 +1,30 @@
 import axios from "axios";
 import React, { Component } from "react";
+import { Container } from './Grid';
 
 class Profile extends Component {
   constructor() {
     super();
     this.state = {
       loggedIn: false,
-      username: null
+      username: null,
+      userInfo: {}
     };
   }
   componentDidMount() {
     axios.get("/user/profile").then(response => {
+      console.log('full response: ' + response);
       console.log("Get user response: ");
       console.log(response.data);
+      console.log('this is the username: ' + response.data.user.username);
+      console.log('this is the frontend info: ' + response.data.user.frontEnd);
       if (response.data.user) {
         console.log("Get User: There is a user saved in the server session: ");
 
         this.setState({
           loggedIn: true,
-          username: response.data.user.username
+          username: response.data.user.username,
+          userInfo: response.data
         });
       } else {
         console.log("Get user: no user");
@@ -32,7 +38,11 @@ class Profile extends Component {
   }
   render() {
     return ( 
-      this.state.loggedIn && <div>Profile</div>  
+      this.state.loggedIn && 
+      <Container>
+      <div>Profile</div>
+      <h1> Hello !</h1>
+      </Container>
     );
   }
 }
