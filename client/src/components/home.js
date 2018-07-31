64831684c4e7521dea4d2709a3fcd29dcfc5bbeb
joglatt <34425753+joglatt@users.noreply.github.com>
2018-07-31 18:57:46 -0400

    navbar

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,66 +1,13 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import { Link } from "react-router-dom";
-import './home.css';
+import React, { Component } from 'react';
 
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  flex: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-};
-
-function ButtonAppBar(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar>
-        <Toolbar>
-          {/* <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton> */}
-          <Typography variant="title" color="inherit" className={classes.flex}>
-            Unify
-          </Typography>
-          <Link to="/user/login" className="btn btn-link text-secondary">
-            <Button className="text-secondary" color="inherit">Login</Button>
-          </Link>
-          <Link to="/user/signup" className="btn btn-link text-secondary">
-            <Button className="text-secondary" color="inherit">Register</Button>
-          </Link>
-          <Link to="/user/profile" className="btn btn-link text-secondary">
-            <Button className="text-secondary" color="inherit">Account</Button>
-          </Link>
-          {/* <SearchBar
-      onChange={() => console.log('onChange')}
-      onRequestSearch={() => console.log('onRequestSearch')}
-      style={{
-        margin: '0 auto',
-        maxWidth: 800
-      }}
-    /> */}
-    
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
 }
 
-ButtonAppBar.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(ButtonAppBar);
+export default Home;
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,27 +1,45 @@
 import React, { Component } from "react";
-// import { Redirect } from 'react-router-dom'
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 import { Link } from "react-router-dom";
+import "./home.css";
 import axios from "axios";
 
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  flex: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+};
 class Navbar extends Component {
   constructor() {
     super();
     this.logout = this.logout.bind(this);
   }
-
   logout(event) {
     event.preventDefault();
     console.log("logging out");
     axios
       .get("/user/logout")
       .then(response => {
-        debugger;
         if (response.status === 200) {
           this.props.updateUser({
             loggedIn: false,
             username: null
           });
-          window.location=response.data.location
+          window.location = response.data.location;
         }
       })
       .catch(error => {
@@ -30,52 +48,83 @@ class Navbar extends Component {
   }
 
   render() {
-    const loggedIn = this.props.loggedIn;
-    console.log("navbar render, props: ");
-    console.log(this.props);
-
+    const { classes, loggedIn } = this.props;
+    // const loggedIn = this.props.loggedIn;
     return (
-      <div>
-        <header className="navbar App-header" id="nav-container">
-          <div className="col-4">
-            {loggedIn ? (
-              <section className="navbar-section">
-                <Link
-                  to="/allusers"
-                  className="btn btn-link text-secondary"
-                  onClick={this.logout}
-                >
-                  <span className="text-secondary">Search</span>
-                </Link>
-                <Link
-                  to="#"
-                  className="btn btn-link text-secondary"
-                  onClick={this.logout}
-                >
-                  <span className="text-secondary">Logout</span>
-                </Link>
-              </section>
-            ) : (
-              <section className="navbar-section">
-                <Link to="/user/home" className="btn btn-link text-secondary">
-                  <span className="text-secondary">home</span>
-                </Link>
-                <Link to="/user/login" className="btn btn-link text-secondary">
-                  <span className="text-secondary">login</span>
-                </Link>
-                <Link to="/user/signup" className="btn btn-link">
-                  <span className="text-secondary">sign up</span>
-                </Link>
-              </section>
-            )}
-          </div>
-          <div className="col-4 col-mr-auto">
-            <div id="top-filler" />
-          </div>
-        </header>
+      <div className={classes.root}>
+        <AppBar>
+          <Toolbar>
+            {/* <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton> */}
+            <Typography
+              variant="title"
+              color="inherit"
+              className={classes.flex}
+            >
+              Unify
+            </Typography>
+            <Link
+              onClick={this.logout}
+              to="#"
+              className="btn btn-link text-secondary"
+            >
+              <Button className="text-secondary" color="inherit">
+                Account
+              </Button>
+            </Link>
+            <Link to="/user/login" className="btn btn-link text-secondary">
+              <Button className="text-secondary" color="inherit">
+                Login
+              </Button>
+            </Link>
+            <Link to="/user/signup" className="btn btn-link text-secondary">
+              <Button className="text-secondary" color="inherit">
+                Register
+              </Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default Navbar;
+// function ButtonAppBar(props) {
+//   const { classes } = props;
+//   return (
+//     <div className={classes.root}>
+//       <AppBar>
+//         <Toolbar>
+//           {/* <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+//             <MenuIcon />
+//           </IconButton> */}
+//           <Typography variant="title" color="inherit" className={classes.flex}>
+//             Unify
+//           </Typography>
+//           <Link to="/user/login" className="btn btn-link text-secondary">
+//             <Button className="text-secondary" color="inherit">
+//               Login
+//             </Button>
+//           </Link>
+//           <Link to="/user/signup" className="btn btn-link text-secondary">
+//             <Button className="text-secondary" color="inherit">
+//               Register
+//             </Button>
+//           </Link>
+//           <Link to="/user/profile" className="btn btn-link text-secondary">
+//             <Button className="text-secondary" color="inherit">
+//               Account
+//             </Button>
+//           </Link>
+//         </Toolbar>
+//       </AppBar>
+//     </div>
+//   );
+// }
+
+Navbar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Navbar);
