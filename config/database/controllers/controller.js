2210ca9056c683eec62de958ac7228ce9c71758d
olivier-dev <omartineau8@gmail.com>
2018-07-31 20:46:56 -0400

    more search logic plus , mergining

--- config/database/controllers/controller.js
+++ config/database/controllers/controller.js
@@ -3,31 +3,38 @@ const router = require("express").Router();
 const db = require("../models");
 
 const userFunctions = {
-  findAll: function(req, res) {
-    db.User.find(req.query)
-      .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  
-  findById: function(req, res) {
-    db.User.findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
+    findAll: function (req, res) {
+        db.User.find(req.query)
+            .sort({date: -1})
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
 
-  update: function(req, res) {
-    db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  
-  remove: function(req, res) {
-    db.User.findById({ _id: req.params.id })
-      .then(dbModel => dbModel.remove())
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  }
+    findById: function (req, res) {
+        db.User.findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+
+    update: function (req, res) {
+        db.User.findOneAndUpdate({_id: req.params.id}, req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+
+    remove: function (req, res) {
+        db.User.findById({_id: req.params.id})
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+
+    find: function(req, res) {
+        console.log(req.query)
+        db.User.find(req.query)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
 };
 
 router.delete("/api/users/:id", userFunctions.remove);
@@ -37,5 +44,7 @@ router.get("/api/users/:id", userFunctions.findById);
 router.get("/api/allusers/", userFunctions.findAll);
 
 router.patch("/api/users/:id", userFunctions.update);
+
+router.get("/api/users", userFunctions.find);
 
 module.exports = router;
