d2f42e50ea28f921822cdd23a45b723158b3279d
Raichel John <raicheljohn28@gmail.com>
2018-08-02 17:05:54 -0400

    icon update

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -3,7 +3,7 @@ import 'typeface-roboto';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import './home.css'
-import DoneIcon from '@material-ui/icons/Done';
+import People from '@material-ui/icons/People';
 
 class Home extends Component {
     render() {
@@ -11,7 +11,7 @@ class Home extends Component {
             <Grid className='homeGrid'> 
                 <Typography>
                     <h1>Welcome to Unify.</h1>
-                    <h2>Where coders connect <DoneIcon/></h2>
+                    <h2>Where coders connect <People/></h2>
                 </Typography>
             </Grid>
         );
