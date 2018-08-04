fcbab955507c9b658e0e78c33135887252a2b0f1
olivier-dev <omartineau8@gmail.com>
2018-08-04 00:23:25 -0400

    adding in some map rendering logic

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,23 +1,57 @@
-import React, { Component } from 'react';
-import 'typeface-roboto';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import './home.css'
-import DoneIcon from '@material-ui/icons/Done';
-import MyMapComponent from './map.js';
-
+import React, { Component } from "react";
+import "typeface-roboto";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import "./home.css";
+import DoneIcon from "@material-ui/icons/Done";
+import MyMapComponent from "./map.js";
+import api from "../api/api";
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: [],
+            markers: []
+        };
+    }
+    loadResults = () => {
+        api
+            .getAllUsers()
+            .then(res => {
+                let result = [];
+                res.data.forEach(user => {
+                    let newMarker = {
+                        long: user.longitude,
+                        lat: user.latitude
+                    };
+                    result.push(newMarker);
+                });
+                console.log(result);
+                this.setState({ markers: result });
+                console.log(res.data);
+            })
+
+            .catch(err => console.log(err));
+    };
+
+
+
+    componentDidMount() {
+        this.loadResults();
+    }
     render() {
         return (
-            <Grid className='homeGrid'>
+            <Grid className="homeGrid">
                 <Typography>
-                    <h1>Welcome to Unify.</h1>
-                    <h2>Where coders connect <DoneIcon /></h2>
+                    <h1>WFelcome to Unify.</h1>
+                    <h2>
+                        Where coders connect <DoneIcon />
+                    </h2>
                 </Typography>
-                <hr/><br />
-                <MyMapComponent isMarkerShown/>
+                <hr />
+                <br />
+                <MyMapComponent markers={this.state.markers} isMarkerShown />
             </Grid>
-
         );
     }
 }
--- client/src/components/map.js
+++ client/src/components/map.js
@@ -2,47 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { compose, withProps } from "recompose";
 import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
+    withScriptjs,
+    withGoogleMap,
+    GoogleMap,
+    Marker
 } from "react-google-maps";
 
 const MyMapComponent = compose(
-  withProps({
-
-    googleMapURL: 
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyChFNgz7xzd9ZSEKcFG4ZlmPOd4tEnkAX8&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ height: `100%` }} />
-  }),
-  withScriptjs,
-  withGoogleMap
+    withProps({
+        googleMapURL:
+            "https://maps.googleapis.com/maps/api/js?key=AIzaSyChFNgz7xzd9ZSEKcFG4ZlmPOd4tEnkAX8&v=3.exp&libraries=geometry,drawing,places",
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: `400px` }} />,
+        mapElement: <div style={{ height: `100%` }} />
+    }),
+    withScriptjs,
+    withGoogleMap
 )(props => (
-  <GoogleMap defaultZoom={8} defaultCenter={{ lat: 40.728157, lng: -74.077644 }}>
-    {props.isMarkerShown && (
-        <div>
-            {/* Following are rending multiple markers */}
-      <Marker position={{ lat: 40.728157, lng: -74.077644 }}
-       />
-       <Marker position={{ lat: 40.730610, lng: -73.935242 }}
-       />
-       <Marker position={{ lat: 40.6870431, lng: -75.21851 }}
-       />
-       <Marker position={{ lat: 41.245708, lng: -75.881241 }}
-       />
-       <Marker position={{ lat: 40.730610, lng: -73.935242 }}
-       />
-       <Marker position={{ lat: 40.3573, lng: -74.6672 }}
-       />
-       <Marker position={{ lat: 39.9526, lng: -75.1652 }}
-       />
-       </div>
-    )}
-  </GoogleMap>
+    <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{ lat: 40.728157, lng: -74.077644 }}
+    >
+        {props.markers.map((marker, i) => (
+            <Marker
+                key={i}
+                position={{ lat: marker.lat, lng: marker.long }}
+            />
+        ))}
+    </GoogleMap>
 ));
 
-<MyMapComponent isMarkerShown />
-
-export default MyMapComponent;
+export default MyMapComponent
