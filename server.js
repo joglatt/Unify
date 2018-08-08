fc5938452583cbcd3227d4a9207a3a9891587639
joglatt <34425753+joglatt@users.noreply.github.com>
2018-08-07 21:22:58 -0400

    updates

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 // const nodemailer = require('nodemailer');
 
 
-
+app.use (express.static("client/build"))
 // MIDDLEWARE
 app.use(morgan('dev'))
 app.use(
